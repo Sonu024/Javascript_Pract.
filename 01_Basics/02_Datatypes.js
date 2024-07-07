281e4ae0b84b876c0870800da35a920d64d92aff
Sonu024 <sonupoudel866@gmail.com>
2024-07-07 19:28:00 +0545

    updated

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,34 @@
+"Use strict"; //treat all JS as newer version
+
+// alert(2+1); we are using nodejs not browser
+
+console.log(2+2)
+console.log("Sonu poudel")
+
+
+/* Different datatypes
+string =""
+boolean= true/false
+number = 
+null
+undefined
+object
+*/
+
+let name= null;
+console.log(typeof null)
+//object
+
+let state;
+console.log(state)
+//undefined
+
+let place="";
+console.log(place)
+//blank space for place
+console.log(typeof place)
+//string
+
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //null
