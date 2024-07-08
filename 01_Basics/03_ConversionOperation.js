1d597cd7cd5aaf0745d226b59be79baf5dee5b8d
Sonu024 <sonupoudel866@gmail.com>
2024-07-08 13:53:13 +0545

    Updated

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -15,3 +15,17 @@ let happy = 1
 console.log(typeof happy)
 let happy_boolean= Boolean(happy)
 console.log(typeof happy_boolean);
+
+
+
+//*****************************prefix and postfix operation***************/
+//postfix : Increment the value but returns value before incrementing
+x=2;
+y=x++;
+console.log(x  , y);
+
+
+//prefix : Increment the value and returns value after incrementing
+x=2;
+y=++x;
+console.log(x  , y);
