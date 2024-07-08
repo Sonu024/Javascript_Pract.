001ea8aedd7150414a9284688b0c22739c8c6347
Sonu024 <sonupoudel866@gmail.com>
2024-07-08 22:45:37 +0545

    updated

--- 01_Basics/summary_daatypes.js
+++ 01_Basics/summary_daatypes.js
@@ -16,11 +16,38 @@ const books = ["Web", "Cryptography"]; //typeof object
 
 //object
 let myObj = {
-    age =45
+   age: 45
 }
 
 const myFunction= function(){                //typeof function
     console.log("Hello World");
 }
 
-console.log(typeof myObj);
+//console.log(typeof );
+
+
+
+
+/***************************Memory types*******************/
+//Premitive datatypes uses stack memory 
+//Non premitive datatypes uses heap memory
+
+let stack1 = "Sonu Chhetri"
+let stack2 = stack1
+stack2 = "Sonu Poudel"
+console.log(stack2);       //Sonu Poudel
+console.log(stack1);       //Sonu Chhetri
+
+//change in value of stack2 only changes the copied value of stack1 but not the original value
+
+let heap1 = {
+   name: "Sonu Chhetri",
+}
+heap2 = heap1
+heap2.name = "Sonu Poudel"
+console.log(heap1.name);    //Sonu Poudel
+console.log(heap2.name);    //Sonu Poudel
+
+//change in value of heap2 changes the original value of heap1
+
+
