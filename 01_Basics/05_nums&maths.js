9f423e155f56ecf85fb0ed01dde709f4e1376f49
Sonu024 <sonupoudel866@gmail.com>
2024-07-10 14:08:54 +0545

    updated

---
+++ 01_Basics/05_nums&maths.js
@@ -0,0 +1,34 @@
+// const score = 600
+// console.log(score);             //600
+
+// const score1 = new Number(100);
+// console.log(score1);             //[Number: 100]
+
+// console.log(score.toFixed(4));     //600.0000
+
+// const value = 123.456
+// console.log(value.toPrecision(4));    //123.5
+// console.log(value.toPrecision(3));    //123
+
+// const value1 = 108798678987
+// console.log(value1.toLocaleString());    //108,798,678,987
+
+
+
+
+// ++++++++++++++++++++++++++Math++++++++++++++++++++++++++++++++
+console.log(Math);
+console.log(Math.random());         //value between 0 and 1
+console.log(Math.random()*10);
+console.log((Math.random()*10) + 1);          //value above 1
+console.log(Math.floor(Math.random()*10) + 1); 
+
+const min = 20;
+const max = 40;
+console.log(Math.floor(Math.random()*(max-min + 1)) + min);
+
+
+
+
+
+
