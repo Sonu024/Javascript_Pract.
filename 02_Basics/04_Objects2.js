ba65140c42602d98e2cb18c9c688ce0bfa760bb6
Sonu024 <sonupoudel866@gmail.com>
2024-07-13 22:58:49 +0545

    updated

---
+++ 02_Basics/04_Objects2.js
@@ -0,0 +1,55 @@
+//const newUser = new Object()
+//const newUser = {}
+//Both are same
+//console.log(newUser);
+
+const newUser = {
+    username: "Sonu",
+    email: "Sonu@gmail.com",
+    address: {
+        pastAddress: "Nawalpur",
+        presentAddress: "Chitwan",
+    }
+}
+//console.log(newUser.address.pastAddress);
+
+
+//Combining objects
+const objA = {
+    a: "A",
+    b: "B",
+}
+const objB = {
+    c: "C",
+    d: "D",
+}
+
+//const objC = Object.assign({}, objA, objB)
+//console.log(objC);                   //{ a: 'A', b: 'B', c: 'C', d: 'D' }
+
+//mostly we use this syntax to combine objects
+const objC = {...objA, ...objB}
+//console.log(objC);
+
+const user = [
+    {
+        id: 10,
+        email: "hb@gmail.com"
+    },
+    {
+        email: "hgvsd@gmail.com"
+    },
+    {
+        email: "hbdsds@gmail.com"
+    }
+]
+//console.log(user[1]);
+
+
+
+console.log(Object.keys( newUser));
+console.log(Object.values( newUser));
+
+
+
+
