2b83a550163c628e59573411596fdec9e519bd16
Sonu024 <sonupoudel866@gmail.com>
2024-07-17 09:33:25 +0545

    updated

---
+++ 03_Basics/04_IIFE.js
@@ -0,0 +1,14 @@
+//Immediately Invoked Function Expression
+//means excute immediately
+//for two invoked function semi-colon is mandatory on first function
+//Named invoked  fhunction and simple invoked function
+
+//named
+// (function me(){
+//     console.log(`Hero`);
+// })();
+
+//unnamed      // passing parameter
+// ((name) => {
+//     console.log(`${name}`)
+// })("Sonu")
