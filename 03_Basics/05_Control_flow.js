ef555cb9ece6691a8d940399d96ee98f82784fe4
Sonu024 <sonupoudel866@gmail.com>
2024-07-19 11:35:07 +0545

    Updated

---
+++ 03_Basics/05_Control_flow.js
@@ -0,0 +1,25 @@
+//if else
+//And
+// const loggedIn = true
+// const fromGoogle = true
+// const fromGmail = false
+
+// if(loggedIn  && fromGmail){
+//     console.log("Accepted");
+// }
+// else{
+//     console.log("Rejected");
+// }
+
+
+//Or
+const loggedIn = true
+const fromGoogle = true
+const fromGmail = false
+
+if(loggedIn  || fromGmail){
+    console.log("Accepted");
+}
+
+
+
