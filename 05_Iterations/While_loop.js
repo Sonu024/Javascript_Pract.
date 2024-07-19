1889fd82ef2715ae6d6aab098534e71b5948f47e
Sonu024 <sonupoudel866@gmail.com>
2024-07-19 19:36:10 +0545

    updated

---
+++ 05_Iterations/While_loop.js
@@ -0,0 +1,24 @@
+//While
+// let i=0
+// while (i<5) {
+//     console.log(`value of i is ${i}`);
+//     i++;
+// }
+
+
+//Array
+// let Array = ["Son", "onu", "on"]
+// let i= 0
+// while (i<Array.length){
+//     console.log(`Value of i: ${Array[i]}`);
+//     i++;
+// }
+
+
+
+//Do While
+let i=0
+do {
+    console.log(`Value of i: ${i}`);
+    i++;
+} while (i<5);
